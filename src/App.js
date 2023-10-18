b7e6bce4abd0826981b967bee8c5396407910c12
Blessing U <blessing986@users.noreply.github.com>
2023-10-18 11:33:16 +0100

    chore: outputting results conditionally

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   const [userInput, setUserInput] = useState(null);
 
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
+    //  Should be triggered when form is submitted
     setUserInput(userInput);
   };
 
-  const yearlyData = []; // per-year results
+  const yearlyData = []; //  per-year results
 
   if (userInput) {
-    let currentSavings = +userInput["current-savings"];
-    const yearlyContribution = +userInput["yearly-contribution"];
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
+    let currentSavings = Number(userInput["current-savings"]);
+    const yearlyContribution = Number(userInput["yearly-contribution"]);
+    const expectedReturn = Number(userInput["expected-return"]) / 100;
+    const duration = Number(userInput["duration"]);
 
-    // The below code calculates yearly results (total savings, interest etc)
+    //  The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
@@ -38,7 +38,17 @@ function App() {
 
       <UserInput onCalculate={calculateHandler} />
 
-      <ResultsTable />
+      {/* dynamically output a fallback text */}
+
+      {!userInput && (
+        <p style={{ textAlign: "center" }}>No investment calculated yet.</p>
+      )}
+      {userInput && (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
     </div>
   );
 }
